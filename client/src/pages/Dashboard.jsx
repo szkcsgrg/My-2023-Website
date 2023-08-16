c0d0e7499383ed1d18e4572aba9e9862392d8ce0
Gergő Szakács <szkcsgrg@gmail.com>
2023-08-16 13:30:25 +0200

    Fixing Layout, Planned CRUD system

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,25 +5,14 @@ function Dashboard() {
     <>
       <div className="section d-flex justify-content-center flex-column col-12">
         <h1>Analitycs</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,
-          maiores rerum assumenda enim nam molestias commodi cumque placeat eos
-          nulla? Iusto molestias fugiat atque nulla doloribus. Consectetur
-          perspiciatis totam mollitia vero! Voluptates atque sapiente
-          repudiandae possimus sequi optio animi laborum at fugit incidunt.
-          Doloremque, officia quis. Architecto repellendus neque sed!
-        </p>
+        <p>Details about the view count interactions etc.</p>
       </div>
       <div className="section d-flex justify-content-center flex-column col-12">
         <h1>Development</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed aperiam
-          ex autem tenetur, ipsum nihil! Tempore adipisci, pariatur obcaecati
-          eaque earum voluptatibus magni tempora sit fugiat eligendi. Blanditiis
-          accusantium veritatis corrupti, culpa nisi quaerat, recusandae
-          officiis quia neque repellat itaque libero. Asperiores quae deleniti
-          non autem. Molestiae voluptatibus nobis dolorem?
-        </p>
+        <h3>Create a Project</h3>
+        <span>To-Do</span>
+        <h3>Edit/Delete Existing Projects</h3>
+        <span>To-Do</span>
       </div>
       <div className="section d-flex justify-content-center flex-column col-12">
         <h1>Photography</h1>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -27,31 +27,38 @@ function Login() {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="landing row d-flex justify-content-center flex-column align-items-center m-0 p-0 m-md-3 p-md-3 m-lg-5 p-lg-5"
     >
       {!userName ? (
-        <div className="col-8 text-center">
-          <h1>Please leave this site!</h1>
-          <p>This login page is only designed for the owner of the website.</p>
-          <p>
-            Trying to login without permission will result trail in the system
-            and an error message for you.
-          </p>
-          <p>
-            Are you lost? Navigate back to the <Link to="/">Home</Link> page!
-          </p>
-          <button onClick={GoogleLogin}>Login with Google</button>
+        <div className="landing row d-flex justify-content-center flex-column align-items-center m-0 p-0 m-md-3 p-md-3 m-lg-5 p-lg-5">
+          <div className="col-8 text-center">
+            <h1>Please leave this site!</h1>
+            <p>
+              This login page is only designed for the owner of the website.
+            </p>
+            <p>
+              Trying to login without permission will result trail in the system
+              and an error message for you.
+            </p>
+            <p>
+              Are you lost? Navigate back to the <Link to="/">Home</Link> page!
+            </p>
+            <button onClick={GoogleLogin}>Login with Google</button>
+          </div>
         </div>
       ) : userEmail === admin ? (
-        <Dashboard />
+        <div className="d-flex flex-column m-0 p-0 m-md-3 p-md-3 m-lg-5 p-lg-5">
+          <Dashboard />
+        </div>
       ) : (
-        <div className="col-8 text-center">
-          <h1>
-            You do not have permission to access this part of the application.
-          </h1>
-          <p>
-            Are you lost? Navigate back to the <Link to="/">Home</Link> page!
-          </p>
+        <div className="landing row d-flex justify-content-center flex-column align-items-center m-0 p-0 m-md-3 p-md-3 m-lg-5 p-lg-5">
+          <div className="col-8 text-center">
+            <h1>
+              You do not have permission to access this part of the application.
+            </h1>
+            <p>
+              Are you lost? Navigate back to the <Link to="/">Home</Link> page!
+            </p>
+          </div>
         </div>
       )}
     </motion.section>
